65e75d9c860101b928f89513857c015e434a1a74
Cursolibras <139021530+Cursolibras@users.noreply.github.com>
2023-08-24 16:45:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,14 +4,27 @@ function mostrarVideo(video) {
       var videoUrl = '';
       if (video === 'video1') {
         videoUrl = 'https://drive.google.com/file/d/10HgDajXrAqwapgqu87PwN1jf76sgKhI4/preview';
-      } else if (video === 'video2') {
-        videoUrl = 'http://drive.google.com/file/d/11aT4z8U-zJrMX_fTGjgK1UgwC5UxQxQW/preview';
-       alert("Nova video aula...");
-      } else if (video === 'video3') {
-        videoUrl = 'https://www.youtube.com/embed/video3';
-      }
+      } 
 
       videoFrame.src = videoUrl;
+
+    var div = document.getElementById("exibirVideo");
+    div.style.display = "block";
+    var navy = document.getElementById("menu");
+    
+    var nav2= document.getElementById("nav");
+    nav2.classList.toggle('active');
+    
+}
+function mostrarVideo2(video2) {
+      var videoFrame2='';
+      videoFrame2 = document.getElementById('videoFrame');
+      var videoUrl2 = '';
+      if (video2 === 'video2') {
+        videoUrl2 = 'http://drive.google.com/file/d/11aT4z8U-zJrMX_fTGjgK1UgwC5UxQxQW/preview';
+        alert("Nova video aula cumprimentos...");
+      } 
+      videoFrame.src = videoUrl2;
     
     
     
@@ -22,7 +35,4 @@ function mostrarVideo(video) {
     var nav2= document.getElementById("nav");
     nav2.classList.toggle('active');
     
-
-
 }
-
